73f823ab977524891d85343d59e2b4a4df46d6a8
bakd247 <rjgcustoms@hotmail.com>
2023-04-02 18:07:55 -0400

    byteAdderOptomized.py
    work just like multiplyNum.py and uses the exact same 256 x 32 grid as a lookup table...but the initialization time is over 400 times faster because the multiplication functions were replaced with addition loops...
    this allows for very fast indexing and key lookup times when compared to recent methods...
    a grid that only uses 15 additions (size 65536 x 16)maximum can be achieved suing this code by just changing the parameters from 254 to 65534 and 32 to 16...
    the intention is to add this as an option later and allow the python tuple object to be saved to disk so that it only needs to be created one time at first initialization.

---
+++ byteAdderOptimized.py
@@ -0,0 +1,66 @@
+from tqdm import tqdm, trange
+from tinyec.ec import SubGroup, Curve
+
+
+name = 'secp256k1'
+p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
+n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+a = 0x0000000000000000000000000000000000000000000000000000000000000000
+b = 0x0000000000000000000000000000000000000000000000000000000000000007
+g = (0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+h = 1
+
+curve = Curve(a, b, SubGroup(p, g, n, h), name)
+pubKey = curve.g*1
+
+numList = []
+for bytePosition in trange(32,total=32,ascii=True,ncols=100,colour='#00ff00',unit='Row',desc='Creating Key LookUp Table...Please Wait:'):
+    A = pubKey * (256 ** bytePosition)
+    numList.append(A)
+tupleNumList = tuple(numList)
+finalList = []
+for tupNum in numList:
+    subList = []
+    zero = 0
+    subList.append(zero)
+    position1 = tupNum
+    subList.append(position1)
+    for iteration in range(254):
+        position2 = position1 + tupNum
+        subList.append(position2)
+        position1 = position2
+    finalList.append(subList)   
+grid = tuple(finalList)
+
+
+def multiplyNum(number):
+    N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+    array = ((number)%N).to_bytes(32, "little")       
+    list = []
+    for byte in array:
+        BBB = int(((hex(byte)))[2:], 16)
+        list.append(BBB)
+    tupleNumber = (tuple(list))
+    posList = []
+    for iteration, place in enumerate(tupleNumber):
+        position = (grid[iteration][place])
+        if position == 0:
+            pass
+        else:
+            posList.append(position)
+    tuplePos = tuple(posList)
+    
+    if len(tuplePos) < 1:
+        print("Infinity and Beyond")
+        
+    else:
+        total = tuplePos[0]
+        if len(tuplePos) < 2:
+            print(total)
+            
+        else:
+            for k in tuplePos[1:]:
+                total = total + k
+            print(total)
+        
+multiplyNum(115792089237316195423570985008687907852837564279074904382605163141518161494336)
