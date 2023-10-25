d87370a5125efa33025bf6db9621fd2f0711a58a
Robert Gordon <rjgcustoms@hotmail.com>
2023-10-24 20:56:07 -0400

    Update hashAdder.py
    added 2 x 3 matrix for all multiples of 2 and 3 up to the amount of input

--- hashAdder.py
+++ hashAdder.py
@@ -1,8 +1,6 @@
 from tinyec.ec import SubGroup, Curve
-from tqdm import trange
-import csv
-import time
-t = time.perf_counter()
+from tqdm import tqdm, trange
+
 print("                                                  ")
 print("                                                  ")
 print("\033[0;32m _               _        _       _     _\033[00m")
@@ -12,25 +10,47 @@ print("\033[0;32m| | | | (_| \__ \ | | |/ ___ \ (_| | (_| |  __/ |\033[00m")
 print("\033[0;32m|_| |_|\__,_|___/_| |_/_/   \_\__,_|\__,_|\___|_|\033[00m")
 print("                                                  ")
 print("                                                  ")
-def sortKeys():
+
+XX = int((input("Please Enter Your Public Key X Coordinate In Hex Format:")),16)
+YY = int((input("Please Enter Your Public Key Y Coordinate In Hex Format:")),16)
+
+def createKey(XX,YY):
     name = 'secp256k1'
     p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
     n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
     a = 0x0000000000000000000000000000000000000000000000000000000000000000
     b = 0x0000000000000000000000000000000000000000000000000000000000000007
-    g = (0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+    g = (XX,YY)
     h = 1
-    curve = Curve(a, b, SubGroup(p, g, n, h), name)
-    PubKey = curve.g*1
-    for multiple in trange(10000,total=10000,ascii=True,ncols=100,colour='#00ff00',unit='KeySort',desc='Sorting Keys'):
-        pubKey = PubKey + PubKey
-        A = str(pubKey.x)[:2]
-        with open('%s.csv' % A, 'a') as e:
-            e.write("%s" % pubKey.x)
-            e.write(str(","))
-            e.write("%s\n" % multiple)
-        PubKey = pubKey
-    elapsed_time = time.perf_counter()          
-    print("                                                  ")
-    print("Finished in" ,elapsed_time-t, "seconds")
-sortKeys()
+    
+    if (((XX * XX * XX)%p) + 7) != ((YY * YY)%p):
+        invalid = ("The Public Key X and Y Coordinates You Entered Are NOT Valid...NOTE: DO NOT Include 02, 03 Or 04 At The Begining Of The X Coordinate And Make Sure You Are Using Hexidecimal Format. If You Need Assistance...Please Contact Technical Support...Please Try Again...Enter Valid X and Y Coordinates For Your Public Key...")
+        return(invalid)
+        exit()
+    else:
+        curve = Curve(a, b, SubGroup(p, g, n, h), name)
+        pubKey= curve.g*1
+        return(pubKey)
+enteredPubKey = createKey(XX,YY)
+pubKeyResult = createKey(XX,YY)
+print("This is the Key you entered in base 10 integer format:",pubKeyResult)
+AA = int(input("Please Enter the Size of the Collision List you would like to Create. Best Performance around 10,000:"))
+print("Creating Lookup table...Please Wait...")
+from wordAdder import multiplyNum
+numList = []
+half = 57896044618658097711785492504343953926418782139537452191302581570759080747169
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+AAA = (AA*2)
+startPubKey = pubKeyResult * ((half ** AA)%N)
+print("Creating Collision List...Please Wait...")
+for numberList in range(AAA):
+    multList = []
+    numList.append(multList)
+for numberedList in numList:
+    Place = startPubKey + startPubKey
+    numberedList.append(Place)
+    for mult in range(AAA):
+        multPlace = Place + Place + Place
+        numberedList.append(multPlace)
+    startPubKey = Place
+print(numList)
