ee64547211348c7d2cb214f9616946fea1ece210
Robert Gordon <rjgcustoms@hotmail.com>
2024-05-07 20:38:28 -0400

    Update hashAdder.py

--- hashAdder.py
+++ hashAdder.py
@@ -33,7 +33,7 @@ pubKeyResult = createKey(XX,YY)
 print("This is the Key you entered in base 10 integer format:",pubKeyResult)
 AA = int(input("Please Enter the Size of the Collision List you would like to Create. Best Performance around 10,000:"))
 print("Creating Lookup table...Please Wait...")
-from byteAdder import multiplyNum
+from wordAdder import multiplyNum
 half = 57896044618658097711785492504343953926418782139537452191302581570759080747169
 N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
 third = 77194726158210796949047323339125271901891709519383269588403442094345440996225
