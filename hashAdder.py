9476af8cb2bdaa2525e1689cb5604ab3212d0c41
Robert Gordon <rjgcustoms@hotmail.com>
2024-05-09 21:38:30 -0400

    Update hashAdder.py
    fixed sorting to allow for indices of each key to not be sorted while remaining with the corresponding key

--- hashAdder.py
+++ hashAdder.py
@@ -2,6 +2,7 @@ from tinyec.ec import SubGroup, Curve
 from tqdm import tqdm, trange
 from os import urandom
 from binarySearch import binarySearch
+from operator import itemgetter
 print("                                                  ")
 print("                                                  ")
 print("\033[0;32m _               _        _       _     _\033[00m")
@@ -33,46 +34,52 @@ pubKeyResult = createKey(XX,YY)
 print("This is the Key you entered in base 10 integer format:",pubKeyResult)
 AA = int(input("Please Enter the Size of the Collision List you would like to Create. Best Performance around 10,000:"))
 print("Creating Lookup table...Please Wait...")
-from byteAdder import multiplyNum
+from wordAdder import multiplyNum
 half = 57896044618658097711785492504343953926418782139537452191302581570759080747169
 N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
 third = 77194726158210796949047323339125271901891709519383269588403442094345440996225
 AAA = (AA**2)
 twosStartPubKey = pubKeyResult * ((half ** (AA))%N)
 print("Creating Collision List...Please Wait...")
-
 numList = []
 for numberList in range(90):
     prefixList = []
-    prefixList.append(int(str(numberList)[:2])+10)
+    prefixList.append(int(str(numberList)[:2])+10) 
     numList.append(prefixList)
-
 twosPositionList = []
 for numberedList in range(AAA):
     Place1 = twosStartPubKey + twosStartPubKey
     twosPositionList.append(Place1)
     twosStartPubKey = Place1
-
 threesList = []
 for twosPlace, twoPosition in enumerate(twosPositionList):
     thirdStartPlace = twoPosition * (third ** AA)
     for thirdMultiple in range(AAA):
         thirdPlaceList = []
+        indicatorList = []
         thirdPlace = thirdStartPlace + thirdStartPlace + thirdStartPlace
         thirdPlaceList.append(thirdPlace.x)
-        thirdPlaceList.append((twosPlace+1)-AA)
-        thirdPlaceList.append((thirdMultiple+1)-AA)
+        indicatorList.append((twosPlace+1)-AA)
+        indicatorList.append((thirdMultiple+1)-AA)
+        tupleIndicatorList = tuple(indicatorList)
+        thirdPlaceList.append(tupleIndicatorList)
         tupleThirdPlaceList = tuple(thirdPlaceList)
         for numedList in numList:
             if numedList[0] != int(str(thirdPlace.x)[:2]):
-                pass
+                pass                                            
             else:
                 numedList.append(tupleThirdPlaceList)
         thirdStartPlace = thirdPlace
-newList = []        
-for listedPrefix in numList:
-    tupleListedPrefix = tuple(listedPrefix)
-    newList.append(tupleListedPrefix)
-newList.sort()
-tupleNewList = tuple(newList)
-print(tupleNewList)   
+for numberedList in numList:
+    numberedList[0] = tuple(numberedList)
+sortedPrefixList = []
+for numsListed in numList:
+    sortedList = sorted(numsListed)
+    tupleSortedList = tuple(sortedList)
+    sortedPrefixList.append(tupleSortedList)
+tupledList = []
+for eachList in numList:
+    tupleEachList = tuple(eachList)
+    tupledList.append(tupleEachList)
+tupleOfTupleList = tuple(tupledList)
+print(tupleOfTupleList)
